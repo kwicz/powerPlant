9361bc6119df3b1f10c5fa5edccbcbe8cd6f9d0f
Katy Solovewicz <katy@solovewicz.com>
2020-04-14 11:51:13 -0700

    add second passing test for all properties
    > Co-authored-by: Mariam <maralaeddin@gmail.com>
    > Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    > Co-authored-by: acolistro <arcolistro@gmail.com>

--- __tests__/powerPlant.test.js
+++ __tests__/powerPlant.test.js
@@ -12,4 +12,22 @@ describe('Plant', () => {
     // Assert
     expect(newFlower.water).toEqual(1);
   });
+
+  test('should correctly adjust all properties of the plant', () => {
+    // Arrange
+    let flower = { soil: 0, light: 0, water: 0 }
+
+    // Act
+    const hydratedFlower = hydrate(1)(flower);
+    const fedFlower = feed(1)(hydratedFlower);
+    const litFlower = giveLight(1)(fedFlower);
+    const buggedOutFlower = giveBug(1)(litFlower);
+    const neglectedFlower = hydrate(-4)(buggedOutFlower);
+
+    // Assert
+    expect(neglectedFlower.water).toEqual(-3);
+    expect(neglectedFlower.soil).toEqual(1);
+    expect(neglectedFlower.light).toEqual(1);
+    expect(neglectedFlower.bug).toEqual(1);
+  });
 });
